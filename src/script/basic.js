5454a409d0cc299f88853dc1815f55dadab7a7f8
cczz <313368171@qq.com>
2017-07-03 18:08:17 +0800

    改动左侧栏

--- src/script/basic.js
+++ src/script/basic.js
@@ -14,3 +14,9 @@ $('.m-topic-module').each((i, el) => {
   let hue = Math.floor(Math.random() * 360)
   $color.css('background', 'hsl(' + hue + ', 40%, 60%)')
 })
+
+$('.m-dropdown.select').on('mousedown', '.list>.item', e => {
+  let t = e.currentTarget.innerText
+  let p = $(e.delegateTarget)
+  p.find('button>.text').text(t)
+})
